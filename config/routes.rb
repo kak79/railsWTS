b818ea974c48fa563a852e5da418719c1c230c2a
kak79 <kohelka@gmail.com>
2020-11-07 18:55:35 -0600

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/login" => "log_in#login"
+  post "/login" => "log_in#post_login"
+  delete "/logout" => "log_in#logout"
+
+  resources :users
+
+
+
+
+
+
 end
