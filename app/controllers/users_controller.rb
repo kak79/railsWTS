6655c1956e1b41ad2bee8f56485125729b44b1cb
kak79 <kohelka@gmail.com>
2020-11-14 11:07:59 -0600

    removed the byebugs, changed ln16 to redirect

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,39 +9,32 @@ class UsersController < ApplicationController
   end
 
   def create
-    #byebug
     @user = User.new(user_params)
     if @user.save
       redirect_to user_path(@user)
     else
-      render :new
+      redirect_to new_user_path
     end
   end
 
   def index
-    # byebug
     @user = User.all
   end
 
   def show
-    #byebug
     if !logged_in?
       redirect_to login_path
     end
-    
   end
 
   def update
-    # byebug
     @user.update(user_params)
     redirect_to user_path(@user)
   end
 
   def destroy
-    # byebug
     @user.destroy
     redirect_to users_path
   end
-
 
 end
