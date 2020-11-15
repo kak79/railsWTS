56b5c5f7f169264640a63b5d288f302154a28fab
kak79 <kohelka@gmail.com>
2020-11-15 16:38:17 -0600

    started the controller actions

---
+++ app/controllers/owners_controller.rb
@@ -0,0 +1,40 @@
+class OwnersController < ApplicationController
+
+  include ApplicationHelper
+
+  before_action :find_owner, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @owner = Owner.new
+  end
+
+  def create
+    @owner = Owner.new(owner_params)
+    if @owner.save
+      redirect_to owner_path(@owner)
+    else
+      redirect_to new_owner_path
+    end
+  end
+
+  def index
+    @owner = Owner.all
+  end
+
+  def show
+    if !logged_in?
+      redirect_to login_path
+    end
+  end
+
+  def update
+    @owner.update(owner_params)
+    redirect_to owner_path(@owner)
+  end
+
+  def destroy
+    @owner.destroy
+    redirect_to owners_path
+  end
+
+end
