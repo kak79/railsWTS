2f691913549a0096e5c9d010048adafb06e97962
kak79 <kohelka@gmail.com>
2020-11-15 08:26:35 -0600

    fixed validates year

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,16 +1,13 @@
 class Car < ApplicationRecord
 
-
   has_one_attached :picture
 
   validates :car_model, presence: true
 
   validates :owner, presence: true
 
-  validates :year, presence: true, numericality: { is: 4 }
+  validates :year, presence: true, numericality: { greater_than_or_equal_to: Date.today.year - 140, less_than_or_equal_to: Date.today.year }, allow_nil: true
 
   validates :description, presence: true, length: { minimum: 25 , maximum: 1000 }
-
-
 
 end
