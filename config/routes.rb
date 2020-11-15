cc0e72419371ee7099c3913961b18c8d592c07cc
kak79 <kohelka@gmail.com>
2020-11-15 16:36:19 -0600

    added to the routes and alphebetized them

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :login, only: [:new, :create, :destroy]
-  resources :users
+
+  resources :campers
   resources :cars
+  resources :login, only: [:new, :create, :destroy]
+  resources :owners
+  resources :users
+
 
 end
