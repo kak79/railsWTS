44351f031152367dba935fdc876053f6409ed2b5
kak79 <kohelka@gmail.com>
2020-11-17 08:13:57 -0600

    removed allow nil pwd

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   validates :user_name, presence: true, uniqueness: true
   
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :password, presence: true, length: { minimum: 6 }
 
   has_one_attached :avatar
 
