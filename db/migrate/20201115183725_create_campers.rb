4feb0e552987169ccb1b4f3b1accab5beaf72088
kak79 <kohelka@gmail.com>
2020-11-18 16:10:24 -0600

    added owner_id

--- db/migrate/20201115183725_create_campers.rb
+++ db/migrate/20201115183725_create_campers.rb
@@ -2,6 +2,7 @@ class CreateCampers < ActiveRecord::Migration[6.0]
   def change
     create_table :campers do |t|
       t.string :name_brand
+      t.integer :owner_id
       t.string :camper_model
       t.integer :year
       t.string :description
