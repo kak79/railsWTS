29f6395df3544bec388694f1cece0fd2ea2267d2
kak79 <kohelka@gmail.com>
2020-11-19 17:10:22 -0600

    dropped the db and migrated

--- db/migrate/20201119182434_add_owner_id_to_cargalleries.rb
+++
@@ -1,5 +0,0 @@
-class AddOwnerIdToCargalleries < ActiveRecord::Migration[6.0]
-  def change
-    add_column :cargalleries, :owner_id, :integer
-  end
-end
--- db/migrate/20201119182505_add_owner_id_to_campgalleries.rb
+++
@@ -1,5 +0,0 @@
-class AddOwnerIdToCampgalleries < ActiveRecord::Migration[6.0]
-  def change
-    add_column :campgalleries, :owner_id, :integer
-  end
-end
