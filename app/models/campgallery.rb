be303e295bba9f78d268b96dd2cfdeeaf1fd996a
kak79 <kohelka@gmail.com>
2020-11-20 09:34:35 -0600

    has_many_attached pictures

--- app/models/campgallery.rb
+++ app/models/campgallery.rb
@@ -2,5 +2,7 @@ class Campgallery < ApplicationRecord
 
   has_many :campers
   has_many :owners, through: :campers
+
+  has_many_attached :pictures
   
 end
--- app/models/cargallery.rb
+++ app/models/cargallery.rb
@@ -3,5 +3,6 @@ class Cargallery < ApplicationRecord
   has_many :cars 
   has_many :owners, through: :cars
 
+  has_many_attached :pictures
   
 end
