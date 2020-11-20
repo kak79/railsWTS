b17cc049083b5a45df8c059974611db83983540b
kak79 <kohelka@gmail.com>
2020-11-20 12:48:02 -0600

    changed name to email uniqueness true

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,9 @@ class User < ApplicationRecord
   
   has_secure_password
 
-  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 
-  validates :user_name, presence: true, uniqueness: true
+  validates :user_name, presence: true
   
   validates :password, presence: true, length: { minimum: 6 }
 
