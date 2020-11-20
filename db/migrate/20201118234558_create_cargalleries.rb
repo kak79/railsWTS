f3abbb75636310dbfd1ce3c050d32a125d4ac429
kak79 <kohelka@gmail.com>
2020-11-20 12:52:04 -0600

    required changes were made

--- db/migrate/20201118234558_create_cargalleries.rb
+++ db/migrate/20201118234558_create_cargalleries.rb
@@ -2,17 +2,8 @@ class CreateCargalleries < ActiveRecord::Migration[6.0]
   def change
     create_table :cargalleries do |t|
       t.integer :car_id
-      t.string :picture_1
-      t.string :description_1
-      t.string :picture_2
-      t.string :description_2
-      t.string :picture_3
-      t.string :description_3
-      t.string :picture_4
-      t.string :description_4
-      t.string :picture_5
-      t.string :description_5
-
+      t.string :description
+      
       t.timestamps
     end
   end
