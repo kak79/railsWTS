1711ebb634065d9954c7774a7d4500698d92b981
kak79 <kohelka@gmail.com>
2020-11-20 12:44:44 -0600

    added an if else that might work to create

--- app/controllers/cargalleries_controller.rb
+++ app/controllers/cargalleries_controller.rb
@@ -11,9 +11,20 @@ class CargalleriesController < ApplicationController
   def create
     @cargallery = @car.cargalleries.new(cargallery_params)
     if @cargallery.save
-      redirect_to owner_car_cargallery_path(@owner,@car,@cargallery)
+      if params[:cargallery]['car_image'].length <= 10
+        params[:cargallery]['car_image'].each do |a|
+           @cargallery = @car.cargallery.create!(:car_image => a, :car_id => @car.id)
+      end
+    elsif params[:cargallery]['car_image'].length > 10
+      flash[:error] = '!'
+      redirect_to ????
+      return false
+      redirect_to @car, notice: "Car Gallery Saved" 
     else
-      redirect_to new_owner_car_cargallery_path(@owner,@car)
+     if params[:cargallery]['car_image'].length > 10
+        @car.errors[:base] << "Maximuim 10 pictures."
+     end
+     redirect_to new_owner_car_cargallery_path(@owner,@car)
     end
   end
 
