3cfe9a09a8812cb281e09c834389156e191313e9
kak79 <kohelka@gmail.com>
2020-11-20 12:47:26 -0600

    changed pictures to car_images

--- app/models/cargallery.rb
+++ app/models/cargallery.rb
@@ -3,6 +3,8 @@ class Cargallery < ApplicationRecord
   has_many :cars 
   has_many :owners, through: :cars
 
-  has_many_attached :pictures
+  has_many_attached :car_images
+
+  validates :description 
   
 end
