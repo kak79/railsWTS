a820a4065752f703a97d0ddee2c6d516e0df0fd8
kak79 <kohelka@gmail.com>
2020-11-24 12:51:58 -0600

    accepts nested attributes for cars&campers

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -3,7 +3,9 @@
 class Owner < ApplicationRecord
   belongs_to :user
   has_many :cars, dependent: :destroy
+  accepts_nested_attributes_for :cars
   has_many :campers, dependent: :destroy
+  accepts_nested_attributes_for :campers  
 
   validates :owner_name, presence: true
 end
